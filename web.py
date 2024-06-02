b997d90bb1b7c871792a682dfb1254c426e5fdd5
Resyorc <blaskishare@gmail.com>
2024-06-02 14:31:24 +0700

    update web.py and app.html

--- web.py
+++ web.py
@@ -19,6 +19,7 @@ def fetch_json(file_path):
 
 file_json = 'website.json'
 data_json = fetch_json(file_json)
+urls = {data_web['url']: 'UNKNOWN' for data_web in data_json['data']}
 
 status_data = {}
 
@@ -28,35 +29,50 @@ async def fetch_status(session, url):
         async with session.get(url, timeout=5) as response:
             return url, 'UP' if response.status == 200 else 'DOWN'
     except (aiohttp.ClientError, asyncio.TimeoutError):
-        return url, 'DOWN'
+        return url, 'UNKNOWN'
     except Exception as e:
         logging.error(f"Error checking {url}: {e}")
         return url, 'DOWN'
 
 
-async def check_website_status():
-    global status_data
+async def check_website_status(url_group):
     async with aiohttp.ClientSession() as session:
-        tasks = [fetch_status(session, url) for url in data_json]
+        tasks = [fetch_status(session, url) for url in url_group]
         results = await asyncio.gather(*tasks)
-        status_data = dict(results)
+        for url, status in results:
+            status_data[url] = status
+
+
+def schedule_checks():
+    up_urls = [url for url, status in status_data.items() if status == 'UP']
+    down_urls = [url for url, status in status_data.items()
+                 if status == 'DOWN']
+    unknown_urls = [url for url, status in status_data.items()
+                    if status == 'UNKNOWN']
+
+    asyncio.run(check_website_status(up_urls))
+    asyncio.run(check_website_status(down_urls))
+    asyncio.run(check_website_status(unknown_urls))
+
 
 scheduler = BackgroundScheduler(timezone=pytz.utc)
 scheduler.add_job(func=lambda: asyncio.run(
-    check_website_status()), trigger="interval", seconds=30)
+    check_website_status(urls.keys())), trigger="interval", seconds=60)
+scheduler.add_job(func=lambda: schedule_checks(),
+                  trigger="interval", seconds=10)
 scheduler.start()
 
 atexit.register(lambda: scheduler.shutdown())
 
 
+@app.route('/api/website', methods=['GET'])
+def get_websites():
+    return jsonify(data_json)
+
+
 @app.route('/api/status', methods=['GET'])
 def get_status():
     return jsonify(status_data)
-
-
-@app.route('/api/website', methods=['GET'])
-def get_website():
-    return jsonify(data_json)
 
 
 @app.route('/')
@@ -65,4 +81,5 @@ def index():
 
 
 if __name__ == "__main__":
+    asyncio.run(check_website_status(urls.keys()))
     app.run(host='0.0.0.0', port=1000)
